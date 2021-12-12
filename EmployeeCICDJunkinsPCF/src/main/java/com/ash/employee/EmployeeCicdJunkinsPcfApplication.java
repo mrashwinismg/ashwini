2f8c12504234e12cb1e85556a579a75b62b0a7d8
ADMIN <ADMIN@DESKTOP-20V94MS>
2021-12-12 15:01:52 +0530

    First Commit

---
+++ EmployeeCICDJunkinsPCF/src/main/java/com/ash/employee/EmployeeCicdJunkinsPcfApplication.java
@@ -0,0 +1,13 @@
+package com.ash.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCicdJunkinsPcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCicdJunkinsPcfApplication.class, args);
+	}
+
+}
---
+++ EmployeeCICDJunkinsPCF/src/test/java/com/ash/employee/EmployeeCicdJunkinsPcfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ash.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCicdJunkinsPcfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
